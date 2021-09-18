1ae83e0cd18458e7e07725d7a656a906201e495d
David Ilie <david.adrian.ilie2005@gmail.com>
2021-09-18 22:51:27 +0100

    Start of activation functions

---
+++ Tests/Mathsy_Projects/ActivationFunctions.c
@@ -0,0 +1,51 @@
+/*
+    A Program which will take an activation function
+    and then display it as a graph between acceptable
+    values of X and Y on the graph.
+
+    Each activation function will return an output
+    for the input that was given. The input value is a
+    value of X which is increasing between two possible
+    values e.g -1 and 1. The output will be used as the 
+    Y coordinate and then a list of tuples (points) which 
+    can then be diplayed onto a graph.
+*/
+
+#include <stdio.h>
+
+#define MAX_X = 1
+#define MIN_X = -1
+
+int main(){
+    return 0;
+}
+
+// Activation Function Here:
+
+int linear(int x){
+    return 0;
+}
+
+int binaryStep(int x){
+    return 0;
+}
+
+int sigmoid(int x){
+    return 0;
+}
+
+int GELU(int x){
+    return 0;
+}
+
+int softplus(int x){
+    return 0;
+}
+
+int swish_1(int x){
+    return 0;
+}
+
+int gaussian(int x){
+    return 0;
+}
